e99ceda966e914d99b33a247d2e1c46e6c7ab316
bahador <noor.bahadory786@gmail.com>
2024-01-27 01:13:32 +0000

    completed for loop exercise

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -55,4 +55,72 @@ const jonasAge = jonas.calcAge();
 
 //console.log(jonasCheckUp);
 
-console.log(jonas.getSummary());
+// console.log(jonas.getSummary());
+
+const jonas1 = [
+  "jonas",
+  "Bahadory",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "peter", "steven"],
+  "meow",
+  true,
+  44,
+];
+
+const types = [];
+
+for (let i = 0; i < jonas1.length; i++) {
+  //console.log(jonas1[i], typeof jonas1[i]);
+  //filling types array
+  // types[i] = typeof jonas1[i];
+
+  types.push(typeof jonas1[i]);
+}
+//console.log(types);
+
+const years = [
+  1995,
+  1997,
+  1998,
+  "hello",
+  true,
+  "morning",
+  false,
+  "how are you doing today?",
+  20000,
+  false,
+];
+const ages = [];
+for (let i = 0; i < years.length; i++) {
+  //console.log(2024 - years[i]);
+  //ages.push(2024 - years[i]);
+  if (typeof years[i] !== "number") continue;
+
+  //console.log(years[i], typeof years[i]);
+}
+
+for (let i = 0; i < years.length; i++) {
+  if (typeof years[i] === "string") break;
+  //console.log(years[i], typeof years[i]);
+}
+
+const testArray = [1, 2, 3, 4, 5];
+for (let i = testArray.length - 1; i >= 0; i--) {
+  //console.log(i, testArray[i]);
+}
+
+for (let exercise = 1; exercise < 3; exercise++) {
+  console.log(`---- starting exercise ${exercise}`);
+
+  for (let workout = 1; workout <= 2; workout++) {
+    console.log(`exercise:${exercise} rep${workout}`);
+
+    for (let i = 1; i <= 2; i++) {
+      console.log(`rep: ${workout} keep going ${i}`);
+      for (let i = 1; i <= 4; i++) {
+        console.log(`${i} push up `);
+      }
+    }
+  }
+}
