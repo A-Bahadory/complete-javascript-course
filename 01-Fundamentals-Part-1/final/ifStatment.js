444022251c10bc15f43f75a4d351d10c6b85a2c8
bahador <noor.bahadory786@gmail.com>
2024-05-27 16:35:07 +0100

    falsy values

---
+++ 01-Fundamentals-Part-1/final/ifStatment.js
@@ -0,0 +1,17 @@
+const ageSarah = 18;
+if (ageSarah >= 19) {
+  console.log(`sarah can drive`);
+} else {
+  const yearsLeft = 19 - ageSarah;
+  console.log(`you can drive in ${yearsLeft} years:`);
+}
+
+const placeOfBirth = "Asia";
+let newPlace;
+if (placeOfBirth == "Asia") {
+  newPlace = "you are asian !";
+} else {
+  newPlace = "you're not from Asia !";
+}
+
+console.log(newPlace);
--- 01-Fundamentals-Part-1/final/workout.js
+++ 01-Fundamentals-Part-1/final/workout.js
@@ -1,24 +1,32 @@
 let x = 10 + 12;
 x = x + 8;
-console.log(x);
+//console.log(x);
 
 let shortWay = 10 + 12;
 shortWay += 8;
-console.log("short way -->", shortWay);
+//console.log("short way -->", shortWay);
 
 let addNewValue = 40 + 33;
 addNewValue = addNewValue + 7;
-console.log(addNewValue);
+//console.log(addNewValue);
 
 let addNewValueShortWay = 40 + 33;
 addNewValueShortWay += 7;
-console.log("short way -->", addNewValueShortWay);
+//console.log("short way -->", addNewValueShortWay);
 addNewValueShortWay *= 2;
-console.log(addNewValueShortWay);
+//console.log(addNewValueShortWay);
 
 addNewValueShortWay /= 4;
-console.log(addNewValueShortWay);
+//console.log(addNewValueShortWay);
 addNewValueShortWay++;
-console.log(addNewValueShortWay);
+//console.log(addNewValueShortWay);
 addNewValueShortWay--;
-console.log(addNewValueShortWay);
+//console.log(addNewValueShortWay);
+
+// get the average
+
+let avFirst = 40;
+let avSe = 60;
+let averageNumber = 100;
+let getAverage = (avFirst + avSe + averageNumber) / 3;
+console.log(getAverage);
