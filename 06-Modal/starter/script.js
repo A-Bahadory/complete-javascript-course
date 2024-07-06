06a758ad45cb6c9f67ca420dac9562a3c75a1526
bahador <noor.bahadory786@gmail.com>
2024-07-06 15:43:08 +0100

    new project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+const querySelector = function (className) {
+  return document.querySelector(className);
+};
+
+const modal = querySelector('.modal');
+const btnCloseModal = querySelector('.close-modal');
+const overlay = querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
